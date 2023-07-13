b11f20d678129bb4541a5bc217f0e5afad2318eb
thomas <tom.angotti11@gmail.com>
2023-07-13 17:30:17 -0600

    testing front

--- main.go
+++ main.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 )
 
+type Items struct {
+	detail string
+	urgent bool
+	completed bool
+}
+
+
+
 func main() {
+
+	// connStr := "postgres://todo_database_k1lt_user:4EOjxkAIA3RvWxyFfAQ6TyNZBNFxbHsb@dpg-cio5v9t9aq06u3msmef0-a.oregon-postgres.render.com/todo_database_k1lt"
+	// // Connect to database
+	// db, err := sql.Open("postgres", connStr)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+
+	itemList := []Items{
+		{"wash dishes", true, false},
+		{"cook dinner", true, true},
+		{"sleep", true, false},
+	}
+
 	app := fiber.New()
 
 	app.Get("/", hello)
+	app.Get("/list", func(c *fiber.Ctx) error {
+		return c.JSON(itemList)
+	})
 
 	app.Get("/env", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, ENV! " + os.Getenv("TEST_ENV"))
@@ -28,3 +55,4 @@ func main() {
 func hello(c *fiber.Ctx) error {
 	return c.SendString("Hello, idiot!")
 }
+
