9309af3ba4e6dcd3b2b1e60cbde26d80667ebbed
thomas <tom.angotti11@gmail.com>
2023-07-13 20:12:22 -0600

    set up routes

--- main.go
+++ main.go
@@ -1,58 +1,159 @@
 package main
 
 import (
-	
+	"database/sql"
 	"log"
-	"os"
-
-	"github.com/gofiber/fiber/v2"
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"fmt"
+	_"github.com/lib/pq"
+	
 )
 
-type Items struct {
-	detail string
-	urgent bool
-	completed bool
+type Todo struct {
+	ID        int
+	Detail     string
+	Completed bool
+	Urgent bool
 }
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "toolman1"
+	dbname   = "mydatabase"
+)
+
+var db *sql.DB
+var err error
+
+func init() {
+	
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 
 func main() {
 
-	// connStr := "postgres://todo_database_k1lt_user:4EOjxkAIA3RvWxyFfAQ6TyNZBNFxbHsb@dpg-cio5v9t9aq06u3msmef0-a.oregon-postgres.render.com/todo_database_k1lt"
-	// // Connect to database
-	// db, err := sql.Open("postgres", connStr)
+	
+	defer db.Close()
+
+	createTableQuery := `
+		CREATE TABLE IF NOT EXISTS todos (
+			id SERIAL PRIMARY KEY,
+			detail TEXT NOT NULL,
+			completed BOOLEAN NOT NULL,
+			urgent BOOLEAN NOT NULL
+		);
+	`
+	// alterTableQuery := `
+	// 		ALTER TABLE todos ADD COLUMN urgent BOOLEAN DEFAULT false
+	// `
+
+
+	_, err = db.Exec(createTableQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// _, err = db.Exec(alterTableQuery)
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
 
+	fmt.Println("Table created successfully")
 
-	itemList := []Items{
-		{detail: "wash dishes", urgent:  true, completed: true},
-		{detail: "cook dinner", urgent: false, completed: true},
-		{detail: "sleep", urgent: false, completed: false},
-	}
+	router := gin.Default()
 
-	app := fiber.New()
+	router.GET("/todos", getTodos)
+	router.POST("/todos", createTodo)
+	router.GET("/todos/:id", getTodo)
+	router.PUT("/todos/:id", updateTodo)
+	router.DELETE("/todos/:id", deleteTodo)
 
-	app.Get("/", hello)
-	app.Get("/list", func(c *fiber.Ctx) error {
-		return c.JSON(itemList)
-	})
-
-	app.Get("/env", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, ENV! " + os.Getenv("TEST_ENV"))
-	})
-
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		port = "3000"
-	}
-
-	log.Fatal(app.Listen("0.0.0.0:" + port))
+	router.Run(":8080")
 }
 
-func hello(c *fiber.Ctx) error {
-	return c.SendString("Hello, idiot!")
+
+func getTodos(c *gin.Context) {
+	rows, err := db.Query("SELECT * FROM todos")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	todos := []Todo{}
+
+	for rows.Next() {
+		todo := Todo{}
+		rows.Scan(&todo.ID, &todo.Detail, &todo.Completed, &todo.Urgent)
+		todos = append(todos, todo)
+	}
+
+	c.JSON(http.StatusOK, todos)
 }
 
+func createTodo(c *gin.Context) {
+	todo := Todo{}
+	err := c.BindJSON(&todo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("INSERT INTO todos (detail, completed, urgent) VALUES ($1, $2, $3)", todo.Detail, todo.Completed, todo.Urgent)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.JSON(http.StatusCreated, todo)
+}
+
+func getTodo(c *gin.Context) {
+	id := c.Param("id")
+
+	todo := Todo{}
+	err := db.QueryRow("SELECT * FROM todos WHERE id = $1", id).Scan(&todo.ID, &todo.Detail, &todo.Completed, &todo.Urgent)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.Status(http.StatusNotFound)
+		} else {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	c.JSON(http.StatusOK, todo)
+}
+
+func updateTodo(c *gin.Context) {
+	id := c.Param("id")
+
+	todo := Todo{}
+	err := c.BindJSON(&todo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("UPDATE todos SET title = $1, completed = $2, urgent = $3 WHERE id = $4", todo.Detail, todo.Completed, todo.Urgent, id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.Status(http.StatusOK)
+}
+
+func deleteTodo(c *gin.Context) {
+	id := c.Param("id")
+
+	_, err := db.Exec("DELETE FROM todos WHERE id = $1", id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.Status(http.StatusOK)
+}
